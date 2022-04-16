e02db773d3f541ace9d5af174dc687b051b19a37
mahfud <makhfudh.taufiqur@gmail.com>
2022-04-16 15:24:07 +0700

    add

--- pages/Posts.js
+++ pages/Posts.js
@@ -1,10 +1,35 @@
-import { useState } from "react"
-import CardPost from "@components/CardPost"
-import Container from "@components/Container"
-import mockPosts from "utils/posts.json"
+import { useState } from "react";
+import SectionHeader from "@components/sectionHeader";
+import CardPost from "@components/CardPost";
+import Container from "@components/Container";
+import Layout from "@components/Layout";
+import mockPosts from "/utils/posts.json"
 
-export default function Posts(mockPosts){
+export default function Posts(){
+    const [posts, setPosts]=useState([]);
     return(
-        <div></div>
-    )
+    <Layout>
+
+      <Container>
+          <SectionHeader>Ui Designer</SectionHeader>
+          {!posts.length?(   
+              <div className="text-center">
+              <h2 className="text-6xl py-2"> No result Bruh</h2>
+              <p className="text-xl">We cant find any posts with keyword, 
+                  please try another keyword</p>
+              </div>  
+          ):(
+        <div className="flex -mx-4 flex-wrap mt-6">
+          {posts.map(post1 => (
+            <div key={post1.id} className="md:w-4/12 px-4 w-full py-5">
+              <CardPost {...post1} />
+              </div>
+          ))}
+          </div>
+          )}
+      </Container>
+    </Layout>
+  );
 }
+
+    
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import FeaturedPost from "@components/FeaturedPost";
 import CardPost from "@components/CardPost";
 import Container from "@components/Container";
 import Layout from "@components/Layout";
-import mockPosts from "../utils/post.json";
+import mockPosts from "utils/posts.json";
 
 
 export default function Home() {
@@ -13,8 +13,8 @@ export default function Home() {
       <Container>
         <FeaturedPost />
         <div className="flex -mx-4 mt-4 flex-wrap">
-          {Posts.map((post) => (
-            <div key={Posts.id} className="md:w-4/12 px-4 w-full py-5">
+          {Posts.map(post => (
+            <div key={post.id} className="md:w-4/12 px-4 w-full py-5">
               <CardPost {...post} />
             </div>
           ))}
